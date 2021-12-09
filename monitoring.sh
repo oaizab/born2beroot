0de7ec8b51bda7699eb486a907482b667d5e2a0e
Aizab Omar <omaraizab98@gmail.com>
2021-12-09 03:14:22 +0100

    script that show system information v2

--- monitoring.sh
+++ monitoring.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 Architecture=`uname -a`
-CPU=`lscpu | grep 'CPU(s):' | cut -d ':' -f2 | tr -d ' ' | awk "NR==1 {print}"`
-vCPU=`cat /proc/cpuinfo | grep 'cpu cores' | cut -d ':' -f2 | tr -d ' ' | awk "NR==1 {print}"`
+CPU=`grep "^physical id" /proc/cpuinfo | uniq | wc -l`
+vCPU=`grep "^processor"  /proc/cpuinfo | uniq | wc -l`
 Mem=`free -m | awk 'NR==2 {printf "%s/%sMB (%.2f%%)",$3,$2,$3*100/$2}'`
 Disk=`df -h | grep "root" | awk '{printf "%d/%s  (%s)",$3,$2,$5}'`
-load=`top -bn1 | grep %Cpu | awk '{printf "%.2f%%", $2 + $4}'`
+load=`top -bn1 | grep %Cpu | awk '{printf "%.1f%%", $2 + $4}'`
 Boot=`who -b | awk '{print $3 " " $4}'`
 LVM=`lsblk | grep lvm &> /dev/null && echo -n "yes" || echo -n "no"`
 TCP=`netstat -an | grep tcp | grep ESTABLISHED | wc -l`
 nUser=`who | awk '{print $1}' | sort -u | wc -l`
 IP=`hostname -I`
 MAC=`ip a | grep ether | awk '{print $2}'`
-SUDO=`wc -l /var/log/sudo/sudo.log | cut -d ' ' -f1 | awk '{print $1 "/2"}' | bc`
+SUDO=`journalctl _COMM=sudo | grep COMMAND | wc -l`
 
 wall "#Architecture: $Architecture
 #CPU physical : $CPU
